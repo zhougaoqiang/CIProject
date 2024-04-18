fde8bfba6b3c9497b7938fe95221f5d2fd7802f3
zhougaoqiang <45629779+zhougaoqiang@users.noreply.github.com>
2024-04-18 16:29:14 +0800

    init

---
+++ 1_removeUnknownAuthorAndIndex.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+columns_to_string = ['Title', 'description', 'image', 'previewLink', 'publisher','publishedDate','infoLink', 'categories']
+
+singleQuotationReplace="&#25"
+doubleQuotationReplace="&#26"
+
+def clean_cell_quotes(df):
+    def clean_cell(value):
+        if isinstance(value,str) and (len(value) > 1):
+            # if value.startswith('"'): #dont know why not work
+            #     value = value[1:]
+            
+            # if value.endswith('"'): #dont know why not work
+            #     value = value[:-1]
+                
+            if any(char in value for char in ('"', "'")):
+                value = value.translate(str.maketrans({'"': doubleQuotationReplace, "'": singleQuotationReplace}))
+        return value
+    return df.applymap(clean_cell)
+
+def read_add_index_save_csv(input_file, output_file):
+  # Read the CSV file using pandas
+  df = pd.read_csv(input_file)
+  #Index,Title,description,authors,image,previewLink,publisher,publishedDate,infoLink,categories,ratingsCount
+  df = df[df['authors'].notna()]
+  df = df[df['Title'].notna()]
+  df = df[df['description'].notna()]
+  df = df[df['categories'].notna()]
+  df = df[df['ratingsCount'].notna()] 
+  # Add an index column starting from 1 and insert it at the first position (loc=0)
+  df.insert(loc=0, column='bookId', value=range(1, len(df) + 1))
+  # df = df.astype({col: str for col in columns_to_string})
+  
+  # df = clean_cell_quotes(df)
+  # Save the dataframe to a new CSV file
+  df.to_csv(output_file, index=False)  # Don't save the index column in the output
+
+# Example usage
+input_file = "./archive/books_raw_data.csv"
+output_file = "./archive/books_data.csv"
+read_add_index_save_csv(input_file, output_file)
---
+++ fileSplit.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+def split_csv_pandas(input_file, output_prefix, chunksize=10000):
+  reader = pd.read_csv(input_file, iterator=True, chunksize=chunksize)
+  file_counter = 0
+  for chunk in reader:
+    output_file = f"{output_prefix}_{file_counter}.csv"
+    # Check if file already exists and create a new filename with a counter if needed
+    while os.path.exists(output_file):
+      file_counter += 1
+      output_file = f"{output_prefix}_{file_counter}.csv"
+    chunk.to_csv(output_file, index=False)
+
+# Example usage
+split_csv_pandas("./archive/books_data.csv", "./archive/books_data", 10000)
