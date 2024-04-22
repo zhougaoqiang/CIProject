9b03afc6556ae7051dc67f049154778a91dc75fe
zhougaoqiang <45629779+zhougaoqiang@users.noreply.github.com>
2024-04-22 22:52:15 +0800

    update

---
+++ 3_import_book_authors.py
@@ -0,0 +1,46 @@
+from neomodel import config
+config.DATABASE_URL = 'bolt://neo4j:OKOKOKOK@localhost:7687'
+
+from neomodel import StructuredNode, StringProperty, IntegerProperty, RelationshipTo, UniqueIdProperty, DateProperty, ArrayProperty, FloatProperty
+
+#bookId,Title,description,authors,image,previewLink,publisher,publishedDate,infoLink,categories,ratingsCount
+
+class Author(StructuredNode) :
+    name = StringProperty(UniqueIdProperty=True, required=True)
+
+class Book(StructuredNode):
+    Title = StringProperty(unique_index=True, required=True)  # 标题作为唯一标识
+    description = StringProperty()
+    image = StringProperty()
+    previewLink = StringProperty()
+    publisher = StringProperty()
+    infoLink = StringProperty()
+    categories = ArrayProperty(StringProperty())  # 分类可能有多个
+    ratingsCount = FloatProperty()
+    authors = RelationshipTo(Author, 'AUTHORED_BY')
+
+import csv
+def importBooks(csvFile):
+    with open(csvFile, 'r', encoding='utf-8') as file :
+        reader = csv.DictReader(file)
+        for row in reader :
+            book = Book(
+                Title=row['Title'],
+                description=row['description'],
+                image=row['image'],
+                previewLink=row['previewLink'],
+                publisher=row['publisher'],
+                infoLink=row['infoLink'],
+                categories=row['categories'].strip("[]").replace("'", "").split(", "),
+                ratingsCount=float(row['ratingsCount'])
+            )
+            book.save()
+
+            authors = row['authors'].strip("[]").replace("'", "").split(", ")
+            for authorName in authors:
+                author_list = Author.get_or_create({'name': authorName})
+                author = author_list[0] if author_list else None
+                if author:
+                    book.authors.connect(author)
+
+importBooks('./archive/books_data.csv')
--- similarity.py
+++ similarity.py
@@ -15,30 +15,51 @@ def sortName(name) :
     sorted_words = sorted(words)
     return " ".join(sorted_words)
 
-# str1 = "zhou Gaoqiang"
-# str2 = "gaoqiang zhou"
-
-# print(checkSimilarity(sortName(str1), sortName(str2)))
-
 import csv
-def extractAuthors(inputFile, outputFile):
+# def extractAuthors(inputFile, outputFile):
+#     nameSet = set()
+#     with open(inputFile, 'r', encoding='utf-8') as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         for row in reader:
+#             authors_list = eval(row['authors'])
+#             for author in authors_list :
+#                 nameSet.add(author)
+                
+#     nameList = [['name', 'sorted_name']]
+#     for name in nameSet :
+#         sortedName = sortName(name)
+#         nameList.append([name, sortedName])
+        
+#     with open(outputFile, 'w', newline='') as writeFile:
+#         writer = csv.writer(writeFile)
+#         writer.writerows(nameList)
+
+def extractAuthors(inputFile, outputFile, similarityThreshold=80):
     nameSet = set()
     with open(inputFile, 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             authors_list = eval(row['authors'])
-            for author in authors_list :
+            for author in authors_list:
                 nameSet.add(author)
-                
-    nameList = [['name', 'sorted_name']]
-    for name in nameSet :
-        sortedName = sortName(name)
-        nameList.append([name, sortedName])
-        
-    with open(outputFile, 'w', newline='') as writeFile:
+
+    nameList = [['name', 'sorted_name', 'similar_names']]
+    sorted_names = {name: sortName(name) for name in nameSet} ##dict
+
+    for name, sorted_name in sorted_names.items() :
+        similar_names = []
+        for other_name in nameSet:
+            if name != other_name :
+                if checkSimilarity(sorted_names[name], sorted_names[other_name]) >= similarityThreshold :
+                    similar_names.append(other_name)
+        print(f"check {name}")
+        nameList.append([name, sorted_name, [", ".join(similar_names)]])
+
+    with open(outputFile, 'w', newline='', encoding='utf-8') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerows(nameList)
+
     
-extractAuthors('./archive/split_data_title_authors.csv', './archive/author_similar.csv')
+extractAuthors('./archive/split_data_title_authors.csv', './archive/author_similar.csv', similarityThreshold=75)
 
 
